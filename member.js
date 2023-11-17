b7617f8ffeb8d72d5ba51c9af32f8427439563c8
Bruno <53633638+brunworks@users.noreply.github.com>
2023-11-17 18:55:01 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsmember() {
+    var member = document.getElementById("member");
+    member.style.display = "block";
+    var member = document.getElementById("member");
+    member.style.display = "none";
+    var member = document.getElementById("member");
+    member.style.display = "none";
+}
